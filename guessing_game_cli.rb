9f0808cd5efc31ef23e02d1bbcca14e4bdd56594
Brian Ly <brian.kyang.ly@gmail.com>
2019-08-05 14:53:34 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,6 +5,7 @@ def welcome
 end
 
 def generate_rand
+<<<<<<< HEAD
   1+rand(1..6)
 end
 
@@ -15,19 +16,38 @@ end
 # def get_user_input
 #   input = gets.chomp
 # end
+=======
+  rand(1..6).to_s
+end
+
+def get_prompt
+  puts "Enter a guess between 1 and 6"
+end
+
+def get_user_input
+  gets.chomp.to_s
+end
+>>>>>>> 7ad558c267e426aa7dc48c7a43f2c1615256211d
 
 def correct_answer
     puts "You guessed the correct number!"
 end
 
+<<<<<<< HEAD
 # def incorrect_answer
 #     puts "Sorry! The computer guessed #{randomg}."
 # end
+=======
+def incorrect_answer(random)
+    puts "Sorry! The computer guessed #{random}."
+end
+>>>>>>> 7ad558c267e426aa7dc48c7a43f2c1615256211d
 
 def exit_command
     puts "Goodbye!"
 end
 
+<<<<<<< HEAD
 def run_guessing_game
   welcome
   randomg = generate_rand
@@ -44,3 +64,23 @@ def run_guessing_game
   end
 end
   
+=======
+
+def run_guessing_game
+
+  welcome
+  r = generate_rand
+  get_prompt
+
+  i = get_user_input
+binding.pry
+  if g == i
+    correct_answer
+  elsif i != "exit" && i != r
+    incorrect_answer(r)
+  elsif i == "exit"
+    exit_command
+  end
+end
+run_guessing_game
+>>>>>>> 7ad558c267e426aa7dc48c7a43f2c1615256211d
